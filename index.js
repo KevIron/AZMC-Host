c2174394f92784c25b66d0061c573842f9cc028c
Marcel Kalużny <92103042+KevIron@users.noreply.github.com>
2025-05-03 15:06:50 +0000

    building the token manager class

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const express = require('express');
+import dotenv from "dotenv"
+import express from "express"
+
+dotenv.config();
+
 const app = express()
 
 app.get("/", (req, res) => {
---
+++ utils/Tokenmanager.js
@@ -0,0 +1,34 @@
+import { env } from "process"
+
+export default class TokenManager {
+    static #token = "";
+    static #expiryDate = 0;
+
+    static a = 0;
+
+    static async #fetchToken() {
+        const requestBody = {
+            scope: "https://management.azure.com//.default",
+            grant_type: "client_credentials",
+            client_id: env.CLIENT_ID,
+            client_secret: env.CLIENT_SECRET,
+        };
+
+        const response = await fetch(`https://login.microsoftonline.com/${env.TENANT_ID}/oauth2/v2.0/token`, {
+            method: "POST",
+            body: new URLSearchParams(requestBody)
+        });
+
+        const resObj = await response.json();
+        
+        this.#token = resObj?.access_token;
+        this.#expiryDate = Date.now() + resObj?.expires_in * 1000;
+
+        this.a += 1;
+    }
+
+    static async getToken() {
+        if (Date.now() >= this.#expiryDate) await this.#fetchToken();
+        return this.#token;
+    }
+}
